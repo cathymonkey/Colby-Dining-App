ae0867532f02e77a264d27e38797949354afb797
点一点功德 up <lyx_computer@tenichitenkudokus-MacBook-Pro.local>
2024-11-04 19:12:45 -0500

    Fix the backref

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ db = SQLAlchemy()
 class Student(db.Model):
     student_email = db.Column(db.String(255), primary_key = True)
     student_access_token = db.Column(db.String(255), unique = True, nullable = False)
-    fav = db.relationship('Favorites', backref = 'stu')
+    fav = db.relationship('Favorites', backref = 'student')
 
 class Food(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -23,17 +23,17 @@ class Favorites(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     created_at = db.Column(db.Date, nullable = False)
     update_at = db.Column(db.Date, nullable = False)
-    student_email = db.Column(db.String(255), db.ForeignKey('stu.student_email'), nullable = False)
+    student_email = db.Column(db.String(255), db.ForeignKey('student.student_email'), nullable = False)
     food_id = db.Column(db.Integer, db.ForeignKey('food.id'), nullable = False)
 
 class Administrator(db.Model):
     admin_email = db.Column(db.String(255), primary_key = True)
     password_hashed = db.Column(db.String(128), nullable = False)
-    feedback = db.relationship('Feedback', backref = 'admin')
+    feedback = db.relationship('Feedback', backref = 'administrator')
 
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    admin_email = db.Column(db.String(255), db.ForeignKey('admin.admin_email'), nullable = False)
+    admin_email = db.Column(db.String(255), db.ForeignKey('administrator.admin_email'), nullable = False)
     content = db.Column(db.Text, nullable = False)
     num_responses = db.Column(db.Integer)
     created_at = db.Column(db.Date, nullable = False)
