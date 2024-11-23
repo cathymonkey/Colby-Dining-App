1496223533bdcc49f9136e16ef27d8505fb64738
cathymonkey <curious.chengyu.f@gmail.com>
2024-11-22 23:31:03 -0500

    test_views

--- tests/functional/test_views.py
+++ tests/functional/test_views.py
@@ -1,4 +1,77 @@
 import pytest
 from website import create_app
+from flask_login import current_user
 from website.models import Student, Food, Tag, Favorites, FeedbackQuestion, Administrator
+from website.views import menu_bp
 
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Test User" in response.data  # Ensure user data is in the response
+
+
+def test_dining_experience(client):
+    response = client.get('/dining-experience')
+
+    assert response.status_code == 200
+
+def test_team(client):
+    response = client.get('/team')
+    
+    assert response.status_code == 200
+
+
+def test_menu(client):
+    response = client.get('/menu')
+
+    assert response.status_code == 200
+
+
+def test_contact(client):
+    response = client.get('/contact')
+
+    assert response.status_code == 200
+
+
+def test_userdashboard(client, current_user):
+    response = client.get('/user/dashboard')
+
+    assert response.status_code == 200
+    assert current_user.username != None
+    assert current_user.email.endswith("@colby.edu")
+
+def test_admindashboard(client):
+
+    response = client.get('/admin/dashboard')
+
+    assert response.status_code == 200
+
+
+def test_logout(client):
+    response = client.get('/logout')
+
+    assert response.status_code == 200
+
+
+
+def test_menu_page(client):
+    pass
+
+def test_get_current_menu(client):
+    pass
+
+def test_submit_feedback(client):
+    pass
+
+
+def test_get_dining_hall_menu(client):
+    pass
+
+
+# testing apis 
+def test_get_weekly_menu(client):
+    pass
+
+def test_get_dining_hours(client):
+    pass
